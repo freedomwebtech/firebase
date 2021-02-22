4d7cbeec126f7a08f567845dfe01762177aac3eb
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-02-22 16:07:10 +0530

    Add files via upload

---
+++ firebase.py
@@ -0,0 +1,28 @@
+import pyrebase
+from distance3 import distance
+
+
+config = {
+    "apiKey": "AIzaSyCZE61YtZWt_lMaZiOq6ANFvLCyrNWYVEQ",
+    "authDomain": "sanket-e9152.firebaseapp.com",
+    "databaseURL": "https://sanket-e9152-default-rtdb.firebaseio.com",
+    "projectId": "sanket-e9152",
+    "storageBucket": "sanket-e9152.appspot.com",
+    "messagingSenderId": "47809698488",
+    "appId": "1:47809698488:web:753c4c7fe364de610e324d"
+    
+
+};
+
+
+firebase = pyrebase.initialize_app(config)
+
+
+
+storage = firebase.storage()
+database = firebase.database()
+a = distance()
+print (a)
+database.child("DB object name")
+data = {"key1": a}
+database.set(data)
